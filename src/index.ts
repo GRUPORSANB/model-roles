3c1f473375853eac94165629586ea795a698d820
Yamil Padron <rsa_yrodriguez@outlook.com>
2021-11-23 15:58:58 +0100

    upd

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { ApplicationUser } from './applicationUser';
+import { VendorUserRole, VendorUserRoleActions, VendorUserRolePermission } from './vendorUserRole';
+
+export {
+    ApplicationUser,
+    VendorUserRole, 
+    VendorUserRoleActions, 
+    VendorUserRolePermission
+};
